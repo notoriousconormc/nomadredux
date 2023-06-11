0493ed57ac692abab54c0bf4eea264d398740196
김나눔 <@naver.com>
2023-06-11 18:07:12 +0900

    switch type etc

--- src/index.js
+++ src/index.js
@@ -6,36 +6,39 @@ import { createStore } from "redux";   //store 나의 data state를 넣는곳
 const add = document.getElementById("add")
 const MINUS = document.getElementById("minus")
 const number = document.querySelector("span")
+
+
 number.innerText = 0 ;
 
-//간단하게 설명 
-// 나의 데이터를 수정할수있는 펑션은 countModifier ,  그래서 나는 밖에서 countModifier와 커뮤니케이션을 해야해 
-// 커뮤니케이션하는방법은 countModifier에 액션을 보내는것임
+const ADD = "ADD";
+const MINUS2 = "MINUS2";
+//이런식으로 타입지정해주면 틀리면 에러떠서 어디가 틀렸는지 금방알수있음 만약에 string를 사용한다면 자바스크립트는 아무 말도 안해주고실행도안될거임 
+
 
 const countModifier = ( count = 0 , action) => {  
-    // 파라미터에 이니셜스테이트 , 엑션 넣어주자  엑션은 리덕스에서 function을 부를때 쓰는 두번째 파라미터 혹은 argument
-    // 5. 메세지는 엑션에 넣고 , 엑션을 체크해보자 
-    if(action.type === "ADD"){
-        // ...count,
-        return count + 1;
-    } else if (action.type === "MINUS"){   //6. else if 다른액션
-        return count -1;
-    } else {    // 7. 다른 액션이 없으면 리턴한다 
-        return count 
+    switch (action.type){  // 액션은 1, 무조건 오브젝트  그리고 2 , 이름은 무조건 type  3, reducer(countModifier)과 소통하는방법
+        case "ADD":
+            return count + 1;
+        case "MINUS2":
+            return count - 1;
+        default:
+            return count;    
     }
-};  //reducer은 함수다 
+}
+
+//action은 countModifier과 소통하는방법 
+//reducer{countModifier}가 리턴하는 것은 무엇이든지 어플리케이션의 스테이트가 된다 
+// dispatch가 리듀서를 불러서 현재 스테이트와 액션을 
+
+
 
 const countStore = createStore(countModifier);  
-// 1 . 우리는 스토어를 먼저만들어야한다 데이터를 넣기위해 , 스토어를 만들면 리듀서도 만들어달라고 한다 여기서 리듀서(countModifier) 
-// 2 . 여기서 reducer == countModifier   ,  데이터는 countModifier에서만 바꿀수있다
-// 
+
 
 
 
 countStore.dispatch({type:"ADD"}); 
 countStore.dispatch({type:"MINUS"}); 
-//  3 . 액션은 object로 정해주세요  {type:"fuck"} 이렇게 
-// 4. dispatch와 액션이 함께 countModifier(reducer)에 메세지를 보낸다 (이런식으로 행동하겠다는) 
 
 
 console.log(countStore)  // 4개의 펀션을 사용함 각자 다른 사용목적
